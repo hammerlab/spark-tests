0b4d447716c9b9a516afeaa5ffb178e855a1c334
Ryan Williams <ryan.blake.williams@gmail.com>
2016-12-04 19:07:04 +0000

    rm unused TestListener module

--- src/main/scala/org/hammerlab/spark/test/listener/RDD.scala
+++
@@ -1,5 +0,0 @@
-package org.hammerlab.spark.test.listener
-
-import org.hammerlab.spark.test.listener.TestSparkListener.RddId
-
-case class RDD(id: RddId, name: String, numPartitions: Int, callSite: String, parents: Seq[RddId])
